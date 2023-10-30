4273e9e0a3bfdfc3addfed9996008e6b8864a6f5
mohid2 <fukahamaroc@gmail.com>
2023-10-30 12:25:21 +0100

    small modifications to the project

--- src/main/java/com/example/ejemploweb/repository/UserRepository.java
+++ src/main/java/com/example/ejemploweb/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.ejemploweb.repository;
 
 import com.example.ejemploweb.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
